1b192e5fd8cf913f77ab0b7a8a9280f6bbf42563
SergRudovich <slunrew@tut.by>
2021-01-09 16:30:25 +0300

    feat: add obstacle class and game functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+/* eslint-disable linebreak-style */
+
+import Player from './player';
+import Obstacle from './obstacle ';
+
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
@@ -10,12 +15,109 @@ let player;
 let gravity;
 let obstacles = [];
 let gameSpeed;
-let keys = {};
+const keys = {};
 
 // Event Listeners
-document.addEventListener('keydown', function (evt) {
+document.addEventListener('keydown', (evt) => {
   keys[evt.code] = true;
 });
-document.addEventListener('keyup', function (evt) {
+document.addEventListener('keyup', (evt) => {
   keys[evt.code] = false;
 });
+
+function SpawnObstacle() {
+  const size = RandomIntInRange(20, 70);
+  const type = RandomIntInRange(0, 1);
+  const obstacle = new Obstacle(canvas.width + size, canvas.height - size, size, size, '#2484E4');
+
+  if (type == 1) {
+    obstacle.y -= player.originalHeight - 10;
+  }
+  obstacles.push(obstacle);
+}
+
+function RandomIntInRange(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function Start() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  ctx.font = '20px sans-serif';
+
+  gameSpeed = 3;
+  gravity = 1;
+
+  score = 0;
+  highscore = 0;
+  if (localStorage.getItem('highscore')) {
+    highscore = localStorage.getItem('highscore');
+  }
+
+  player = new Player(25, 0, 50, 50, '#FF5858');
+
+  scoreText = new Text(`Score: ${score}`, 25, 25, 'left', '#212121', '20');
+  highscoreText = new Text(`Highscore: ${highscore}`, canvas.width - 25, 25, 'right', '#212121', '20');
+
+  requestAnimationFrame(Update);
+}
+
+const initialSpawnTimer = 200;
+let spawnTimer = initialSpawnTimer;
+function Update() {
+  requestAnimationFrame(Update);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  spawnTimer -= 1;
+  if (spawnTimer <= 0) {
+    SpawnObstacle();
+    console.log(obstacles);
+    spawnTimer = initialSpawnTimer - gameSpeed * 8;
+
+    if (spawnTimer < 60) {
+      spawnTimer = 60;
+    }
+  }
+
+  // Spawn Enemies
+  for (let i = 0; i < obstacles.length; i++) {
+    const o = obstacles[i];
+
+    if (o.x + o.w < 0) {
+      obstacles.splice(i, 1);
+    }
+
+    if (
+      player.x < o.x + o.w
+        && player.x + player.w > o.x
+        && player.y < o.y + o.h
+        && player.y + player.h > o.y
+    ) {
+      obstacles = [];
+      score = 0;
+      spawnTimer = initialSpawnTimer;
+      gameSpeed = 3;
+      window.localStorage.setItem('highscore', highscore);
+    }
+
+    o.Update();
+  }
+
+  player.Animate();
+
+  score += 1;
+  scoreText.t = `Score: ${score}`;
+  scoreText.Draw();
+
+  if (score > highscore) {
+    highscore = score;
+    highscoreText.t = `Highscore: ${highscore}`;
+  }
+
+  highscoreText.Draw();
+
+  gameSpeed += 0.003;
+}
+
+Start();
---
+++ src/obstacle .js
@@ -0,0 +1,25 @@
+/* eslint-disable linebreak-style */
+export default class Obstacle {
+  constructor(x, y, w, h, c) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.c = c;
+
+    this.dx = -gameSpeed;
+  }
+
+  Update() {
+    this.x += this.dx;
+    this.Draw();
+    this.dx = -gameSpeed;
+  }
+
+  Draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.c;
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+    ctx.closePath();
+  }
+}
